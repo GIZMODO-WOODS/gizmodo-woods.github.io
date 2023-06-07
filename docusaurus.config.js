a302dceb300ad01e46c96dedd36466fc26439af5
こもれび <hobbypopkechupki@gmail.com>
2023-06-08 00:12:37 +0900

    Correct page edit links.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,14 +42,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/GIZMODO-WOODS/gizmodo-woods.github.io/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/GIZMODO-WOODS/gizmodo-woods.github.io/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/GIZMODO-WOODS/gizmodo-woods.github.io/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/GIZMODO-WOODS/gizmodo-woods.github.io/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
