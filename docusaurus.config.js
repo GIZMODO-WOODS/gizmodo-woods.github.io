62e9a23348a7afd3f76d6b1a54e7b54e0d498729
ヨミ <hobbypopkechupki@gmail.com>
2023-06-08 13:25:31 +0900

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -42,14 +42,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/GIZMODO-WOODS/gizmodo-woods.github.io/',
+            'https://github.com/GIZMODO-WOODS/gizmodo-woods.github.io/brob/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/GIZMODO-WOODS/gizmodo-woods.github.io/',
+            'https://github.com/GIZMODO-WOODS/gizmodo-woods.github.io/brob/main/',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
