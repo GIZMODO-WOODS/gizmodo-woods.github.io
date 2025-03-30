4aa97bf6dc3dbdbee282caeb81d1baecc5f4af6c
Hima <131072934+Hima-Zinn@users.noreply.github.com>
2025-03-30 10:55:32 +0900

    エラー修正

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -45,15 +45,16 @@ const config = {
   ],
 
   themes: [
-    require.resolve("@easyops-cn/docusaurus-search-local"),
-      ({
-          hashed: true,
-          indexPages: true,
-          language: ["ja", "en"],
-          removeDefaultStemmer: true,
-          highlightSearchTermsOnTargetPage: true,
-          fuzzyMatchingDistance: 2,
-      }),
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      {
+        hashed: true,
+        indexPages: true,
+        language: ["ja", "en"],
+        removeDefaultStemmer: true,
+        highlightSearchTermsOnTargetPage: true,
+        fuzzyMatchingDistance: 2,
+      },
     ],
   ],
 
